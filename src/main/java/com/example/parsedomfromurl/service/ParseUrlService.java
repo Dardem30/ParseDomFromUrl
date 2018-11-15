4c7202e13512bae1ebc3f89662e543a68e139638
Roman <rlukashenko@cygno.com>
2018-11-15 21:04:54 +0300

    Base master

--- src/main/java/com/example/parsedomfromurl/service/ParseUrlService.java
+++ src/main/java/com/example/parsedomfromurl/service/ParseUrlService.java
@@ -14,16 +14,7 @@ public class ParseUrlService {
 
     public String parseUrl(String inUrl) {
         try {
-            Document doc = Jsoup.connect(inUrl).get();
-            String host = URI.create(inUrl).getHost();
-            Elements links = doc.select("a[href]");
-            StringBuilder result = new StringBuilder("Рекламные ссылки: \n");
-            links.stream()
-                    .map(link -> link.attr("href"))
-                    .filter(hrefUrl -> hrefUrl.contains("http"))
-                    .filter(hrefUrl -> !host.equals(URI.create(hrefUrl).getHost()))
-                    .forEach(hrefUrl -> result.append("    ").append(hrefUrl).append("\n"));
-            return result.toString();
+            return inUrl;
         } catch (Exception e) {
             logger.error("Error during parsing url", e);
             return "Exception";
